843bb4e1621e260e508f711d8c764c701e46b974
sgrunden <savannah.grunden@gmail.com>
2018-12-05 15:00:02 -0800

    Ship nose material and Rocket Control Script

---
+++ Assets/Rocket.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ProcessInput();
+	}
+
+    private void ProcessInput()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            print("Space Pressed.");
+        }
+    }
+}
