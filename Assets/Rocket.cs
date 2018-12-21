bd0792626b47c7121b517b7ce8c24d946b21a140
sgrunden <savannah.grunden@gmail.com>
2018-12-21 15:33:17 -0800

    Thruster color, thrust fuxed

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -7,41 +7,51 @@ public class Rocket : MonoBehaviour {
 
     Rigidbody rigidBody;
     AudioSource audioSource;
-	
-	void Start () {
+
+    [SerializeField] float rcsThrust = 100f;
+    [SerializeField] float mainThrust = 100f;
+
+    void Start () {
         rigidBody = GetComponent<Rigidbody>();
         audioSource = GetComponent<AudioSource>();
 	}
 	
 
 	void Update () {
-        ProcessInput();
-	}
+        Rotate();
+        Thrust();
+    }
 
-    private void ProcessInput()
+    private void Thrust()
     {
-        if(Input.GetKey(KeyCode.Space)) 
+        if (Input.GetKey(KeyCode.Space))
         {
-            rigidBody.AddRelativeForce(Vector3.up); // always in the direction that the ship is facing
-            if(!audioSource.isPlaying)
+            rigidBody.AddRelativeForce(Vector3.up * mainThrust); // always in the direction that the ship is facing
+            if (!audioSource.isPlaying)
             {
                 audioSource.Play();
             }
-            print("Space Pressed.");
         }
         else
         {
             audioSource.Stop();
         }
+    }
+
+    private void Rotate()
+    {
+        rigidBody.freezeRotation = true; // take manual control of rotation
+   
+        float rotationThisFrame = rcsThrust * Time.deltaTime; // time of last frame, good estimate for current frame
+
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(Vector3.forward);
-            print("Rotating Left.");
-        } 
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.Rotate(-Vector3.forward);
-            print("Rotating Right.");
+            transform.Rotate(Vector3.forward * rotationThisFrame);
         }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
+        }
+        rigidBody.freezeRotation = false; //resume physics control of rotation
     }
 }
